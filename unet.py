d542704e75ea01306fccd28715452c0c7720d64f
jasonjnie <jasonniejx@gmail.com>
2018-05-03 17:30:54 -0500

    verbose 1

--- unet.py
+++ unet.py
@@ -161,7 +161,7 @@ class myUnet(object):
 
         model_checkpoint = ModelCheckpoint('./results/unet.hdf5', monitor='loss',verbose=1, save_best_only=True)
         print('Fitting model...')
-        model.fit(imgs_train, imgs_mask_train, batch_size=4, epochs=10, verbose=0,validation_split=0.2, shuffle=True, callbacks=[model_checkpoint])
+        model.fit(imgs_train, imgs_mask_train, batch_size=4, epochs=10, verbose=1,validation_split=0.2, shuffle=True, callbacks=[model_checkpoint])
 
         print('predict test data')
         imgs_mask_test = model.predict(imgs_test, batch_size=1, verbose=1)
