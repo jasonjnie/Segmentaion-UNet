5be40eabc785eca69be55f52a769e6efabf437e4
jasonjnie <jasonniejx@gmail.com>
2018-05-03 17:52:48 -0500

    debug

--- unet.py
+++ unet.py
@@ -156,9 +156,9 @@ class myUnet(object):
 
         print("loading data")
         imgs_train, imgs_mask_train, imgs_test, imgs_val = self.load_data()
-        print('train data size =', imgs_train.shape)
-        print('val data size =', imgs_val.shape)
-        print('test data size =', imgs_val.shape)
+        print('train data size =', imgs_train)
+        print('val data size =', len(imgs_val))
+        print('test data size =', len(imgs_val))
 
         print("loading data done")
         model = self.get_unet()
