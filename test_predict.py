29d4837ce9327e091a94025a4756c3f64748dad1
jasonjnie <jasonniejx@gmail.com>
2018-05-05 16:30:37 -0500

    updated test for c1

--- test_predict.py
+++ test_predict.py
@@ -2,17 +2,14 @@ from unet import *
 from data import *
 
 mydata = dataProcess(512,512)
-
+mydata.test_path = "./data/c1"
+mydata.create_test_data()       # create test data for c3
 imgs_test = mydata.load_test_data()
 
 myunet = myUnet()
-
 model = myunet.get_unet()
-
 model.load_weights('./results/unet.hdf5')
-
 imgs_mask_test = model.predict(imgs_test, batch_size=5, verbose=1)
-
 np.save('./results/imgs_mask_test.npy', imgs_mask_test)
 
 for i in range(imgs_mask_test.shape[0]):
