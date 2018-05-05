59a7e3927dc18a136db367ecf8542875435bd977
jasonjnie <jasonniejx@gmail.com>
2018-05-05 16:21:00 -0500

    save fig in test

--- test_predict.py
+++ test_predict.py
@@ -14,3 +14,9 @@ model.load_weights('./results/unet.hdf5')
 imgs_mask_test = model.predict(imgs_test, batch_size=5, verbose=1)
 
 np.save('./results/imgs_mask_test.npy', imgs_mask_test)
+
+for i in range(imgs_mask_test.shape[0]):
+    img = imgs_mask_test[i]
+    print('184:', img.max(), img.min())
+    img = array_to_img(img)
+    img.save("./results/%d.jpg" % (i))
